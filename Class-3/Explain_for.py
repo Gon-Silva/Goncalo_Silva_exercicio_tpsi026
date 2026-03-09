467a7a1a39e8feace0a81e02b11da67283e8fde2
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-03-09 21:35:22 +0000

    Adicionei a explicação do for

---
+++ Class-3/Explain_for.py
@@ -0,0 +1,11 @@
+# Loop For and While
+
+names = ["João", "Pedro", "Antonio"]
+
+# Normal for
+for name in names:
+    print(name)
+
+# Function in range
+for i in range(len(names)):
+      print(names[i])
