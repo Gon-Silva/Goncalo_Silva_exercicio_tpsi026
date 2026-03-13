cfe0467354e24d1b2e6e666ebd5939997bc859e2
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-03-13 21:55:16 +0000

    Adicionei o exercício da converção do tempo

---
+++ Exercises/Ex_If/Convert_time.py
@@ -0,0 +1,39 @@
+import os
+import platform
+
+# This function clean the console
+# For this, I need to import modulos like « os » and « platform »
+
+    # os permit to interact with operating system
+    # plataform permit the program to undertand what type of operating system the user is using
+
+# Function to clean the console
+def clear_console():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Function to print a header
+def print_header():
+    print("========================================")
+    print("             CONVERT TIME               ")
+    print("========================================")
+
+def main():
+    clear_console()
+
+    print_header()
+
+    print("\n > Insert the seconds")
+    second = int(input("  > "))
+
+    hour = int(second / 3600)
+    minutes = int(second / 60 - hour * 60)
+    second = second - (hour * 3600 + minutes * 60)
+
+    print(f"{hour} hour, {minutes} minutes, {second} second\n")
+
+# Start the program
+if __name__ == "__main__":
+    main()
