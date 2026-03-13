a6981f0784b8acde2b00884e0d74dcc2bb394674
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-03-13 22:27:45 +0000

    Adicionei o segundo exercícios da ficha IF

---
+++ Exercises/Ex_If/Find_biggest_number.py
@@ -0,0 +1,64 @@
+import os
+import platform
+
+# This function clean the console
+# For this, I need to import modulos like « os » and « platform »
+
+    # os permit to interact with operating system
+    # plataform permit the program to undertand what type of operating system the user is using
+
+# Function to clean the console
+def clear_console():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Function to print a header
+def print_header():
+    print("========================================")
+    print("        FIND THE BIGGEST NUMBER         ")
+    print("========================================")
+
+# Function that sort the array
+def sort_array(arr):
+    size = len(arr)
+    temp = arr[0]
+
+    for i in range(size):
+        for j in range(0, size - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1] 
+                arr[j + 1] = temp
+
+    return arr
+
+
+def main():
+    clear_console()
+
+    print_header()
+
+    # Create a array
+    arr = []
+
+    print("\n > Insert the first number")
+    arr.append(int(input("  > ")))
+
+    print("\n > Insert the second number")
+    arr.append(int(input("  > ")))
+
+    print("\n > Insert the third number")
+    arr.append(int(input("  > ")))
+
+    sort_array(arr)
+
+    print(f"\n > The biggest is {arr[len(arr) - 1]}")
+    print(f" > The lowest is {arr[0]}")
+
+    print("")
+
+# Start the program
+if __name__ == "__main__":
+    main()
