5848ee6e581ee18bcb029c2f457fdcda32066597
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-04-09 17:39:55 +0100

    Adição do exercício 2 sobre dicinários

---
+++ Exercises/Ex_Dic/Ex2.py
@@ -0,0 +1,12 @@
+def print_model(dic: dict):
+    print(f"{dic['model']}")
+
+def main():
+
+    # Dictonary
+    car = {"brand": "Toyota", "model": "Corolla", "year": 2020}
+
+    print_model(car)
+
+if __name__ == "__main__":
+    main()
